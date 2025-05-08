4b31b9a1f9a5b8af1040ab32cf07e820c70f73dc
Antony Messerli <antony@mes.ser.li>
2025-05-07 20:18:46 -0500

    Update build.sh

--- builds/rockylinux-9/build.sh
+++ builds/rockylinux-9/build.sh
@@ -9,7 +9,7 @@ docker build --platform=linux/amd64 -t rocky-9-fips .
 docker run --platform=linux/amd64 --rm -ti \
 -v "$PWD"/build:/tmp/auroraboot \
 -v /var/run/docker.sock:/var/run/docker.sock \
-quay.io/kairos/auroraboot:v0.5.0 \
+quay.io/kairos/auroraboot:v0.7.0 \
 --set container_image=docker://rocky-9-fips \
 --set "disable_http_server=true" \
 --set "disable_netboot=true" \
